4075a292951302e5eec64072c0ee7fe88644d486
Osherhacohen <osherhacohen@gmail.com>
2023-12-28 14:00:55 +0200

    exporting context

--- src/services/context/ExampleContext.js
+++ src/services/context/ExampleContext.js
@@ -3,9 +3,10 @@ import { createContext } from "react";
 //Create a new context and assign its initial values, we would usually want these empty.
 //Eventually, we would want to connect the context to a custom hook that will be able 
 //to provide a "global" hook under all of the context provider's children.
-export const ExampleContext = createContext({
+const initialContext = {
     hookValue1: "value1",
     functionExample1: () => { },
     functionExample2: () => { },
-});
+}
+export const ExampleContext = createContext(initialContext);
 
